b5584a1b2e47aa3d8b515ecac71f0788fac74f95
Zvyagincev <A.V.Zvyagincev@yandex.ru>
2020-08-30 12:32:59 +0300

    Убрал мусор

--- app/src/main/java/com/sss/breter/voicerecognizer/MainActivity.java
+++ app/src/main/java/com/sss/breter/voicerecognizer/MainActivity.java
@@ -1,51 +1,28 @@
 package com.sss.breter.voicerecognizer;
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Scanner;
-
+import android.content.Context;
 import android.os.AsyncTask;
 
 import com.sss.breter.voicerecognizer.recognizer.UnityAssets;
 import com.unity3d.player.UnityPlayer;
-import com.unity3d.player.UnityPlayerActivity;
 
-import edu.cmu.pocketsphinx.*;
+import java.io.File;
+import java.io.IOException;
 
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
-public class MainActivity extends UnityPlayerActivity implements RecognitionListener {
-
-    // *********** секция взаимодествия с Unity *************
-    /**
-     * имя объекта принимающего callback из этой библиотеки
-      */
-    private static String _recieverObjectName = null;
-    /**
-     * имя метода принимающего callback-log из этой библиотеки
-     */
-    private static String _logReceiverMethodName = null;
-    /**
-     * имя метода принимающего callback с результатом распознавания речи
-     */
-    private static String _recognitionResultMethodNameReciever = null;
-    /**
-     * имя метода принимающего колбэк о завершении инициализиции распознователя
-     */
-    private static String _initializationCompleteMethodNameReciever = null;
-    /**
-     * имя метода принимающего колбэк с сообщением об ошибках
-     */
-    private static String _crashMessRecieverMethodName = null;
+public class MainActivity implements RecognitionListener {
     /**
      * устанавливаем имя Unity объекта - приёмника сообщений из данной библиотеки
      * @param name                     - имя объекта
      */
     public static void setRecieverObjectName( String name ){
+        toUnityLog("setRecieverObjectName " + name);
         _recieverObjectName = name;
     }
-
     /**
      * устанавливаем имя метода Unity объекта - метода вывода в лог Unity
      * @param name  -   имя метода
@@ -53,72 +30,21 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
     public static void setLogReceiverMethodName( String name ){
         _logReceiverMethodName = name;
     }
-
     /**
      * устанавливаем имя метода Unity объекта - обработчика результатов распознавания
      * @param name  -   имя метода
      */
     public static void setRecognitionResultRecieverMethod( String name ) { _recognitionResultMethodNameReciever = name; }
-
     /**
      * устанавливаем имя метода Unity объекта - обработчика сообщения об окончании инициализации распознавателя
      * @param name - имя метода
      */
     public static void setInitializationCompleteMethod( String name ) { _initializationCompleteMethodNameReciever = name; }
-
     /**
      * устанавливаем имя метода Unity объекта - обработчика сообщения об ошибке в работе библиотеки
      * @param name
      */
     public static void setCrashMessageRecieverMethod( String name ) { _crashMessRecieverMethodName = name; }
-    /**
-     * отправка в Unity сообщения для вывода в лог
-     * @param message - сообщение
-     */
-    private static void toUnityLog( String message ) {
-        if ( ( _recieverObjectName != null ) && ( _logReceiverMethodName != null ) )
-            UnityPlayer.UnitySendMessage( _recieverObjectName, _logReceiverMethodName, message );
-    }
-
-    /**
-     * отправка в Unity сообщения с результатами распознавания
-     * @param recognitionResult - результат распознавания в рамках последней сессии
-     */
-    private static void regonitionResultToUnity( String recognitionResult ) {
-        if ( ( _recieverObjectName != null ) && ( _recognitionResultMethodNameReciever != null ) )
-            UnityPlayer.UnitySendMessage( _recieverObjectName, _recognitionResultMethodNameReciever, recognitionResult );
-    }
-    /**
-     * отправка в Unity сообщения с результатами инициализации mRecognizer
-     * @param message
-     */
-    private static void initializationResult( String message ) {
-        if ( ( _recieverObjectName != null ) && ( _initializationCompleteMethodNameReciever != null ) )
-            UnityPlayer.UnitySendMessage( _recieverObjectName, _initializationCompleteMethodNameReciever, message );
-    }
-
-    /**
-     * отправка в UNity сообщения об ошибке в работе библиотеке
-     * @param crash
-     */
-    private static void crashMessToUnity( String crash ) {
-        if ( ( _recieverObjectName != null ) && ( _crashMessRecieverMethodName != null ) )
-            UnityPlayer.UnitySendMessage( _recieverObjectName, _crashMessRecieverMethodName, crash );
-    }
-    // ******************************************************
-
-    private SpeechRecognizer _mRecognizer;
-
-    private static final String ACOUSTIC_MODELS_DIR = "acousticModels/";
-    private static final String KEYPHRASE_SEARCH    = "keyphrase_search";
-    private static final String INIT_RESULT_TRUE    = "true";
-    private static final String INIT_RESULT_FALSE    = "false";
-
-    private String _baseGrammarName = null;
-    private int _timeoutInterval = 1000;
-    private boolean _useKeyword = false;
-    private float _threshold = 1e-45f;
-
     /**
      * Начинаем распознавание речи
      */
@@ -127,12 +53,12 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
             crashMessToUnity( "Recognizer is null" );
             return;
         }
+        Boolean start = false;
         if ( _useKeyword )
-            _mRecognizer.startListening( KEYPHRASE_SEARCH );
+            start = _mRecognizer.startListening( KEYPHRASE_SEARCH );
         else
-            _mRecognizer.startListening( _baseGrammarName, _timeoutInterval );
+            start = _mRecognizer.startListening( _baseGrammarName, _timeoutInterval );
     }
-
     /**
      * Прекращаем распознвание речи
      */
@@ -144,17 +70,17 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
      * Конфигурируем mRecognizer
      * @param language - язык
      */
-    public void runRecognizerSetup( String language ) {
+    public void runRecognizerSetup( String language ) throws IOException {
+        toUnityLog( "runRecognizerSetup" );
         language = ACOUSTIC_MODELS_DIR + language;
         final String selectedLanguage = language;
-
         new AsyncTask<Void, Void, File>( ) {
             @Override
             protected File doInBackground( Void... params ) {
                 File assetDir = null;
                 try {
                     toUnityLog( "Acoustic model is being loaded..." );
-                    UnityAssets assets = new UnityAssets( MainActivity.this, selectedLanguage );
+                    UnityAssets assets = new UnityAssets( _context, selectedLanguage );
                     assetDir = assets.syncAssets( );
                 } catch ( IOException e ) {
                     crashMessToUnity( e.getMessage( ) );
@@ -180,25 +106,20 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
             }
         }.execute( );
     }
-
     /**
      * устанавливает временной интервал сессии распознавания
      * @param interval  -   временной интервал в м.с.
      */
-    public void setTimeoutInterval( int interval )
-    {
+    public void setTimeoutInterval( int interval ) {
         _timeoutInterval = interval;
     }
-
     /**
      * устанавливает базовый файл грамматики инициализируемый после распознования ключевого слова
      * @param grammarFileName - имя файла грамматики
      */
-    public void setBaseGrammarFile( String grammarFileName )
-    {
+    public void setBaseGrammarFile( String grammarFileName ) {
         _baseGrammarName = grammarFileName;
     }
-
     /**
      * Переключаем файл грамматики для mRecognizer
      * @param searchName - ключ файла грамматики
@@ -226,6 +147,9 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
     public MainActivity( ) throws IOException {
 
     }
+    public MainActivity( Context context ) throws IOException {
+        _context = context;
+    }
     /**
      * движок услышал какой-то звук, может быть это речь (а может быть и нет)
      */
@@ -233,7 +157,6 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
     public void onBeginningOfSpeech( ) {
         //toUnityLog("start of speech");
     }
-
     /**
      * микрофон больше не фиксирует звуки
      */
@@ -241,7 +164,6 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
     public void onEndOfSpeech( ) {
         //toUnityLog("end of speech");
     }
-
     /**
      * есть промежуточные результаты распознавания. Для активационной фразы это значит, что она сработала.
      * Аргумент Hypothesis содержит данные о распознавании (строка и score)
@@ -264,20 +186,16 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
      */
     @Override
     public void onResult( Hypothesis hypothesis ) {
-        //toUnityLog("onResult");
         if ( hypothesis != null ) {
             regonitionResultToUnity( hypothesis.getHypstr( ) );
         }
     }
-
     @Override
     public void onError( Exception e ) {
         crashMessToUnity( e.getMessage( ) );
     }
-
     @Override
     public void onTimeout( ) {
-        //toUnityLog( "timeout" );
         switchSearch( _baseGrammarName );
     }
     /**
@@ -288,7 +206,6 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
      */
     public boolean addWordIntoDictionary( String word, String phones ) {
         try {
-            //toUnityLog( "try add word:" + pWord + " phones:" + pPhones );
             if (_mRecognizer.getDecoder( ) != null)
                 _mRecognizer.getDecoder( ).addWord( word, phones, 1 );
             else
@@ -308,7 +225,6 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
      */
     public boolean addGrammarString( String grammarName, String grammarString ) {
         try {
-            //toUnityLog( "try add grammar string:" + pGrammarString );
             if ( _baseGrammarName == null ) _baseGrammarName = grammarName;
             _mRecognizer.addGrammarSearch( grammarName, grammarString );
         }
@@ -360,4 +276,70 @@ public class MainActivity extends UnityPlayerActivity implements RecognitionList
     public void setThreshold( float threshold ) {
         _threshold = threshold;
     }
+    /**
+     * отправка в Unity сообщения для вывода в лог
+     * @param message - сообщение
+     */
+    private static void toUnityLog( String message ) {
+        if ( ( _recieverObjectName != null ) && ( _logReceiverMethodName != null ) )
+            UnityPlayer.UnitySendMessage( _recieverObjectName, _logReceiverMethodName, message );
+    }
+    /**
+     * отправка в Unity сообщения с результатами распознавания
+     * @param recognitionResult - результат распознавания в рамках последней сессии
+     */
+    private static void regonitionResultToUnity( String recognitionResult ) {
+        if ( ( _recieverObjectName != null ) && ( _recognitionResultMethodNameReciever != null ) )
+            UnityPlayer.UnitySendMessage( _recieverObjectName, _recognitionResultMethodNameReciever, recognitionResult );
+    }
+    /**
+     * отправка в Unity сообщения с результатами инициализации mRecognizer
+     * @param message
+     */
+    private static void initializationResult( String message ) {
+        if ( ( _recieverObjectName != null ) && ( _initializationCompleteMethodNameReciever != null ) )
+            UnityPlayer.UnitySendMessage( _recieverObjectName, _initializationCompleteMethodNameReciever, message );
+    }
+    /**
+     * отправка в UNity сообщения об ошибке в работе библиотеке
+     * @param crash
+     */
+    private static void crashMessToUnity( String crash ) {
+        if ( ( _recieverObjectName != null ) && ( _crashMessRecieverMethodName != null ) )
+            UnityPlayer.UnitySendMessage( _recieverObjectName, _crashMessRecieverMethodName, crash );
+    }
+
+    private SpeechRecognizer _mRecognizer;
+
+    private static final String ACOUSTIC_MODELS_DIR = "acousticModels/";
+    private static final String KEYPHRASE_SEARCH    = "keyphrase_search";
+    private static final String INIT_RESULT_TRUE    = "true";
+    private static final String INIT_RESULT_FALSE    = "false";
+
+    private String _baseGrammarName = null;
+    private int _timeoutInterval = 1000;
+    private boolean _useKeyword = false;
+    private float _threshold = 1e-45f;
+
+    private Context _context;
+    /**
+     * имя объекта принимающего callback из этой библиотеки
+     */
+    private static String _recieverObjectName = null;
+    /**
+     * имя метода принимающего callback-log из этой библиотеки
+     */
+    private static String _logReceiverMethodName = null;
+    /**
+     * имя метода принимающего callback с результатом распознавания речи
+     */
+    private static String _recognitionResultMethodNameReciever = null;
+    /**
+     * имя метода принимающего колбэк о завершении инициализиции распознователя
+     */
+    private static String _initializationCompleteMethodNameReciever = null;
+    /**
+     * имя метода принимающего колбэк с сообщением об ошибках
+     */
+    private static String _crashMessRecieverMethodName = null;
 }
